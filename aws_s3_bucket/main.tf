e70e824c5b8e192320450c75647e7870fdf6fb3e
mohiniphatak <phatakmohini@gmail.com>
2024-03-20 11:47:35 +0530

    Update main.tf

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -9,15 +9,4 @@ terraform {
 resource "aws_s3_bucket" "test" {
   bucket = "terraformgithubactions-yeeeekkk"
 }
-#   versioning {
-#     enabled = true
-#   }
 
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
