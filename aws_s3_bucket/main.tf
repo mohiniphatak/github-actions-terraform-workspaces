4bfd09463e1b4612f0930022435412f8c12f0816
phatakmohini@gmail.com <phatakmohini@gmal.com>
2024-03-20 12:40:28 +0530

    prod bucket adeed

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "aws_s3_bucket" "test" {
-  # bucket = var.environment+""
-  bucket    = "${var.environment}.my-bucket-yeeeeeeeeeee"
+  bucket = "some-shitty-bucket-name-qunitw"
+  # bucket    = "${var.environment}.my-bucket-yeeeeeeeeeee"
 }
 
